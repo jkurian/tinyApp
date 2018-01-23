269e5c08629b1395e08f7bf5e7ae6efd2c52317e
Jerry Kurian <jkurian770@gmail.com>
2018-01-23 12:34:17 -0500

    Added a String.prototype.hashCode function which generates a random hash value which is converted to a hex number and returned.
    Wrote a generateRandomString function which takes in a url and returns a random string which is a hex number

--- express_server.js
+++ express_server.js
@@ -1,42 +1,60 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 8080; // default port 8080
-const bodyParser = require("body-parser");
+// const express = require("express");
+// const app = express();
+// const PORT = process.env.PORT || 8080; // default port 8080
+// const bodyParser = require("body-parser");
 
-app.set("view engine", "ejs")
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+// app.set("view engine", "ejs")
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
 
-var urlDatabase = {
-    "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com"
-};
+// var urlDatabase = {
+//     "b2xVn2": "http://www.lighthouselabs.ca",
+//     "9sm5xK": "http://www.google.com"
+// };
 
-app.get('/urls', (req, res) => {
-    let templateVars = {
-        urlList: urlDatabase
-    };
-    res.render("urls_index", templateVars);
-});
+// app.get('/urls', (req, res) => {
+//     let templateVars = {
+//         urlList: urlDatabase
+//     };
+//     res.render("urls_index", templateVars);
+// });
 
-app.get("/urls/new/", (req, res) => {
-    res.render("urls_new");
-});
+// app.get("/urls/new/", (req, res) => {
+//     res.render("urls_new");
+// });
 
-app.get("/urls/:id", (req, res) => {
-    let templateVars = {
-        tinyURL: req.params.id,
-        urlDatabase: urlDatabase
-    };
-    res.render("url_show", templateVars);
-});
+// app.get("/urls/:id", (req, res) => {
+//     let templateVars = {
+//         tinyURL: req.params.id,
+//         urlDatabase: urlDatabase
+//     };
+//     res.render("url_show", templateVars);
+// });
 
-app.post("/urls", (req, res) => {
-    console.log(req.body); // debug statement to see POST parameters
-    res.send("Ok"); // Respond with 'Ok' (we will replace this)
-});
+// app.post("/urls", (req, res) => {
+//     console.log(req.body); // debug statement to see POST parameters
+//     res.send("Ok"); // Respond with 'Ok' (we will replace this)
+// });
 
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}!`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Example app listening on port ${PORT}!`);
+// });
+
+function generateRandomString(url) {
+    return url.hashCode();
+}
+
+String.prototype.hashCode = function () {
+    var hash = 0;
+    var rand = Math.floor(Math.random()*100);
+    if (this.length == 0) return hash;
+    for (i = 0; i < this.length; i++) {
+        char = this.charCodeAt(i);
+        rand = Math.floor(Math.random()*100);
+        hash = ((hash << 5) - hash) + char - rand;
+        hash = Math.abs(hash & hash); // Convert to 32bit integer
+    }
+    return hash.toString(16);
+}
+console.log(generateRandomString("www.gmail.com"));
