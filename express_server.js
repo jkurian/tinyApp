3b336e2734c87a712857209da2f11ce8e1d1b3a9
Jerry Kurian <jkurian770@gmail.com>
2018-01-23 12:13:49 -0500

    Added a new route to /urls/new which allows the user to generate a new tiny-url. The urls_new.ejs renders a form which can submit a url via POST which is sent to app.post(...) in express_server.js

--- express_server.js
+++ express_server.js
@@ -1,8 +1,12 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 var urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
@@ -13,17 +17,25 @@ app.get('/urls', (req, res) => {
     let templateVars = {
         urlList: urlDatabase
     };
-
     res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new/", (req, res) => {
+    res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
-    let templateVars = { 
-        tinyURL: req.params.id, 
+    let templateVars = {
+        tinyURL: req.params.id,
         urlDatabase: urlDatabase
-     };
+    };
     res.render("url_show", templateVars);
-  });
+});
+
+app.post("/urls", (req, res) => {
+    console.log(req.body); // debug statement to see POST parameters
+    res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
