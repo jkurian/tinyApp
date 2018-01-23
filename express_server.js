88b555a2abf7d8fdc25fbd8aa2d8ceb560a4ee02
Jerry Kurian <jkurian770@gmail.com>
2018-01-23 12:42:56 -0500

    removed uncommented code

--- express_server.js
+++ express_server.js
@@ -1,45 +1,45 @@
-// const express = require("express");
-// const app = express();
-// const PORT = process.env.PORT || 8080; // default port 8080
-// const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
 
-// app.set("view engine", "ejs")
-// app.use(bodyParser.urlencoded({
-//     extended: true
-// }));
+app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-// var urlDatabase = {
-//     "b2xVn2": "http://www.lighthouselabs.ca",
-//     "9sm5xK": "http://www.google.com"
-// };
+var urlDatabase = {
+    "b2xVn2": "http://www.lighthouselabs.ca",
+    "9sm5xK": "http://www.google.com"
+};
 
-// app.get('/urls', (req, res) => {
-//     let templateVars = {
-//         urlList: urlDatabase
-//     };
-//     res.render("urls_index", templateVars);
-// });
+app.get('/urls', (req, res) => {
+    let templateVars = {
+        urlList: urlDatabase
+    };
+    res.render("urls_index", templateVars);
+});
 
-// app.get("/urls/new/", (req, res) => {
-//     res.render("urls_new");
-// });
+app.get("/urls/new/", (req, res) => {
+    res.render("urls_new");
+});
 
-// app.get("/urls/:id", (req, res) => {
-//     let templateVars = {
-//         tinyURL: req.params.id,
-//         urlDatabase: urlDatabase
-//     };
-//     res.render("url_show", templateVars);
-// });
+app.get("/urls/:id", (req, res) => {
+    let templateVars = {
+        tinyURL: req.params.id,
+        urlDatabase: urlDatabase
+    };
+    res.render("url_show", templateVars);
+});
 
-// app.post("/urls", (req, res) => {
-//     console.log(req.body); // debug statement to see POST parameters
-//     res.send("Ok"); // Respond with 'Ok' (we will replace this)
-// });
+app.post("/urls", (req, res) => {
+    console.log(req.body); // debug statement to see POST parameters
+    res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
-// app.listen(PORT, () => {
-//     console.log(`Example app listening on port ${PORT}!`);
-// });
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
+});
 
 function generateRandomString() {
     var rand = Math.floor(Math.random()*100000000).toString();
