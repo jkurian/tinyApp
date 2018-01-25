e795129b6944dc94597cd76357e5d792f6f7bcc5
Jerry Kurian <jkurian770@gmail.com>
2018-01-25 16:13:12 -0500

    Added comments and bootstrap

--- express_server.js
+++ express_server.js
@@ -1,52 +1,45 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8080; // default port 8080
-const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 8080;
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser')
 const bcrypt = require('bcrypt');
 const cookieSession = require('cookie-session')
 
-app.use(express.static('public'))
-app.set("view engine", "ejs")
-app.use(cookieParser());
-app.use(cookieSession({
-    name: 'session',
-    keys: ['key1', 'key2', 'key3'],
-
-    // Cookie Options
-    maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}));
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-var urlDatabase = {
+//Stores all our users tiny URLS
+const urlDatabase = {
     userRandomID: {
-        "b2xVn2": "http://www.lighthouselabs.ca",
-        "9sm5xK": "http://www.google.com"
+        'b2xVn2': 'http://www.lighthouselabs.ca',
+        '9sm5xK': 'http://www.google.com'
     },
     user2RandomID: {
-        "test": "test.come"
+        'test': 'test.come'
     }
 };
+
+//stores all our users information
+//The two currently present are for testing only
 const users = {
-    "userRandomID": {
-        id: "userRandomID",
-        email: "user@example.com",
-        password: bcrypt.hashSync("purple-monkey-dinosaur", 10)
+    'userRandomID': {
+        id: 'userRandomID',
+        email: 'user@example.com',
+        password: bcrypt.hashSync('1', 10)
     },
-    "user2RandomID": {
-        id: "user2RandomID",
-        email: "user2@example.com",
-        password: bcrypt.hashSync("dishwasher-funk", 10)
+    'user2RandomID': {
+        id: 'user2RandomID',
+        email: 'user2@example.com',
+        password: bcrypt.hashSync('2', 10)
     }
 }
 
+//generates a random string which is used as a key
 function generateRandomString() {
     var rand = Math.floor(Math.random() * 100000000).toString();
     return rand.hashCode();
 }
 
+//default hash function implemented in javas .toHash function
+//returns a string from the hashed value
 String.prototype.hashCode = function () {
     var hash = 0;
     if (this.length == 0) return hash;
@@ -59,80 +52,154 @@ String.prototype.hashCode = function () {
     return hash.toString(32);
 }
 
-function findUser(req) {
-    console.log("FInding user logged");
-    if(req.session.user_id) {
-        return true;
+//Check if user is currently logged in
+function checkLoggedIn(req, res, next) {
+    if (req.path.match(/login|register/)) {
+        next();
+        return;
     }
-}
-
-function checkUserLoginMiddleware(req, res, next) {
-    if(req.path.match(/login|register/) || findUser(req)) {
-        next()
+    const currentUser = req.session.user_id;
+    if (currentUser) {
+        next();
     } else {
-        res.status(403).redirect('http://localhost:8080/login/')
+        let errorMessage = {
+            message: 'Please login first!',
+            sendTo: '/login'
+        }
+        res.render('errors', errorMessage);
     }
 }
 
-app.use(checkUserLoginMiddleware);
-/*
- * Routes below 
- */
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+app.use(cookieParser());
+app.use(cookieSession({
+    name: 'session',
+    keys: ['key1', 'key2', 'key3'],
+
+    // Cookie Options
+    maxAge: 2 * 60 * 60 * 1000 // 2 hours
+}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(checkLoggedIn);
+
+
+
+//ROUTES BEGIN
+
+// Home page, if user is logged in --> redirect to /urls
+// if user is not logged in --> redirect to login
+app.get('/', (req, res) => {
+    if (req.session.user_id) {
+        res.redirect('/urls')
+    } else {
+        res.redirect('login')
+    }
+})
+
+//render the urls_index.ejs file whenever we try to access /urls
+// Do not need to check if user is logged in because middleware does
+// that for us and redirects the user to log in.
 app.get('/urls', (req, res) => {
-    console.log("Rendering...", req.session.user_id);
     let templateVars = {
         urlList: urlDatabase[req.session.user_id],
         user: users[req.session.user_id]
     };
-    res.render("urls_index", templateVars);
+    res.render('urls_index', templateVars);
 });
 
+//handle a post form sent to /urls. Essentially generates a new URL,
+//adds this url/tinurl to the urlDarabase and redirects the user to /urls/tinyURL.
+app.post('/urls', (req, res) => {
+    var tinyURL = generateRandomString();
+    urlDatabase[req.session.user_id][tinyURL] = req.body.longURL;
+    res.status(302).redirect(`http://localhost:8080/urls/${tinyURL}`);
+});
+
+//if the user is logged in, then we redirect them to /urls
+//if the user is not logged in, then we render the login page
 app.get('/login', (req, res) => {
-    delete req.session.user_id;
-    res.render("login");
+    if (req.session.user_id) {
+        res.redirect('urls');
+    }
+    res.render('login');
 });
 
+//Checks if the user email and login infomation match, if it does then 
+//we set the request session user_id to the users id found in the user database
+//If there were problems authenticating the user, we render an
+//error.ejs page with an appropriate error message and a link to go 
+//back to the login page
 app.post('/login', (req, res) => {
-    // console.log('attempting');
     delete req.session.user_id;
     let attemptLogin;
     let flag = false;
+    //check each user email and find the user object that belongs to that email
     for (let user in users) {
         if (users[user].email === req.body.email) {
-            console.log("found you!")
-            console.log("ID is: ", users[user]);
             attemptLogin = users[user];
             flag = true;
             break;
         }
     }
+    //if the flag is true, then we found the user object
     if (!flag) {
-        return res.status(403).send();
+        let errorMessage = {
+            message: 'Sorry, your login information is incorrect.',
+            sendTo: 'login'
+        }
+        res.render('errors', errorMessage);
     }
-    // console.log(req.body);
+    //if the password inputted and password of the user object do not match
+    //then we render an error page with the appropriate error message
     if (!bcrypt.compareSync(req.body.password, attemptLogin.password)) {
-        return res.status(403).send();
+        let errorMessage = {
+            message: 'Sorry, your login information is incorrect.',
+            sendTo: 'login'
+        }
+        res.render('errors', errorMessage);
     }
+    //if we get here, the user password and email have matched and we redirect the user
+    //to the urls page
     req.session.user_id = attemptLogin.id;
-    console.log("logging in", req.session.user_id, attemptLogin.id);
-    console.log(users);
-    res.redirect("http://localhost:8080/urls/");
-    //res.cookie('user_id', attemptLogin.id).redirect("http://localhost:8080/urls/");
+    res.redirect('http://localhost:8080/urls/');
 });
 
+//when the register page is accessed with a GET request.
+//If the user is already logged in, we redirect them to urls
+// If not, we redirect them to register
 app.get('/register', (req, res) => {
-    delete req.session.user_id;
+    if (req.session.user_id) {
+        res.redirect('urls');
+    }
     res.render('register');
 });
+
+//When the user presses the submit button on the register page, we first clear
+//the cookie in the session and then check that the email or password fields are
+// not empty. If they are empty we render the errors.ejs page with the appropriate message.
+//Next we check if the user email has already been registered. If not,
+// then we create a new user and add it to the user database and also create
+// an object for the user in the urlDatabase to track their short URLs.
 app.post('/register', (req, res) => {
-    //console.log(users);
     delete req.session.user_id;
     if (!req.body.email || !req.body.password) {
-        return res.status(400).send();
+        let errorMessage = {
+            message: 'Do not leave anything blank....',
+            sendTo: 'register'
+        }
+        res.render('errors', errorMessage);
     }
     for (let user in users) {
         if (users[user].email === req.body.email) {
-            return res.status(400).send();
+            let errorMessage = {
+                message: 'That email is taken!',
+                sendTo: 'register'
+            }
+            res.render('errors', errorMessage);
         }
     }
     let userID = generateRandomString();
@@ -142,83 +209,109 @@ app.post('/register', (req, res) => {
         password: bcrypt.hashSync(req.body.password, 10)
     }
     urlDatabase[userID] = {};
-    console.log("user_id = ", userID);
     req.session.user_id = userID;
-    console.log("user registered: ", users);
-    console.log("urlDatabase updated: ", urlDatabase);
-    // res.cookie("user_id", userID);
-    res.status(301).redirect("http://localhost:8080/urls/");
+    res.status(301).redirect('http://localhost:8080/urls/');
 });
 
-app.get("/urls/new/", (req, res) => {
+//If there is a GET to urls/new, we first check if the user is logged in.
+//If not logged in, we redirect them to the login page or else we render
+//the urls_new page with the user object passed to the urls_new.ejs
+app.get('/urls/new/', (req, res) => {
     if (req.session.user_id === undefined) {
-        res.status(403).render("login");
+        res.status(403).redirect('login');
     }
     let templateVars = {
         user: users[req.session.user_id]
     }
 
-    res.status(200).render("urls_new", templateVars);
+    res.status(200).render('urls_new', templateVars);
 });
 
-app.get("/u/:shortURL", (req, res) => {
-    //if the requested URL doesnt exist
-    //LAST CHANGE
-    // console.log(req.params.shortURL);
-    // console.log("Here");
+//When using the tinyURL, if the GET request is for a tinyURL that has not
+//been generated, then we render the error.ejs page with the appropriate message
+//If the tinyURL is present in the urlDatabase, then we redirect the user to
+//the longURL assosciated with that key
+app.get('/u/:shortURL', (req, res) => {
     if (!urlDatabase[req.session.user_id][req.params.shortURL]) {
-        console.log('That is not a valid tiny-url!');
-        // res.status(303).send('Invalid URL. Please double check the URL.');
-        res.status(302).redirect('/urls/new');
+        let errorMessage = {
+            message: 'That tinyURL does not exist!',
+            sendTo: 'urls'
+        }
+        res.status(302).render('errors', errorMessage);
     } else {
         let longURL = urlDatabase[req.session.user_id][req.params.shortURL];
-        // console.log("redirect to: ", longURL)
         res.redirect(longURL);
     }
 });
 
-app.post("/urls", (req, res) => {
-    // console.log(req.body); // debug statement to see POST parameters
-    // console.log(req.cookies);
-    console.log("urlDatabase: ", urlDatabase);
-    var tinyURL = generateRandomString();
-    urlDatabase[req.session.user_id][tinyURL] = req.body.longURL;
-    console.log("URL added!", urlDatabase)
-    // console.log("Database updated\n", urlDatabase);
-    // TODO: CHECK STATUS CODE
-    res.status(302).redirect(`http://localhost:8080/urls/${tinyURL}`);
-});
-
-app.get("/urls/:id", (req, res) => {
-
+//first we get the usersURLs from the urlDatabase
+//If this is undefined, then that user is not in the urlDatabase
+//so we send them to the errors.ejs page and tell them to log in first
+//If it is not undefined, then we have to check if the url they are trying
+//to access is in their urlsDatabase object. If it is not,
+//then we tell them they have not created that tinyURL
+//Otherwise we render the url_show.ejs page with the correct information
+app.get('/urls/:id', (req, res) => {
+    var usersURLs = urlDatabase[req.session.user_id];
+    if (usersURLs === undefined) {
+        var errorMessage = {
+            message: 'Please login first!',
+            sendTo: '/'
+        }
+        return res.render('errors', errorMessage);
+    }
+    //TODO: Review this piece of code
+    if (!usersURLs.hasOwnProperty(req.params.id)) {
+        var errorMessage = {
+            message: 'You have not created that tinyURL!',
+            sendTo: '/'
+        }
+        return res.render('errors', errorMessage);
+    }
     let templateVars = {
         tinyURL: req.params.id,
         urlDatabase: urlDatabase[req.session.user_id],
         user: users[req.session.user_id]
     };
-    res.render("url_show", templateVars);
+    res.render('url_show', templateVars);
 });
 
-app.post("/urls/:id", (req, res) => {
-    // console.log(req.params.id, req.body.newURL);
+//When a user tries to POST a new URL to the database, we first check if 
+//they are logged in, if not send them to errors.ejs.
+//If they are logged in we get their orresponding urlDatabase object associated 
+//to that person and find the URL associated to that hash and we update the value
+//of it to the new URL the user wants to link to.
+app.post('/urls/:id', (req, res) => {
+    if(!req.sessions.user_id) {
+        var errorMessage = {
+            message: 'You have not created that tinyURL!',
+            sendTo: '/'
+        }
+        return res.render('errors', errorMessage);
+    }
     urlDatabase[req.session.user_id][req.params.id] = req.body.newURL;
 
     res.status(301).redirect('http://localhost:8080/urls/');
 });
 
-app.post("/urls/:id/delete", (req, res) => {
-    // console.log(req.cookies["user_id"]);
+//If the user clicks the delete button on url_index.ejs, then we delete
+//that entry in the urlDatabase associated to that user with the specified hash
+//We have a setTimeout here to show the popup before the page redirects
+app.post('/urls/:id/delete', (req, res) => {
     delete urlDatabase[req.session.user_id][req.params.id];
     setTimeout(function () {
         res.status(301).redirect('http://localhost:8080/urls/')
     }, 1000);
 });
 
-app.post("/logout", (req, res) => {
+//if the user clicks the logout button, then we delete the cookie and redirect them
+//to login
+app.post('/logout', (req, res) => {
     delete req.session.user_id;
-    res.status(301).redirect('http://localhost:8080/urls/');
+    res.status(301).redirect('http://localhost:8080/login');
 });
 
+//Listening to the specified port
 app.listen(PORT, () => {
     console.log(`tinyApp listening on port ${PORT}!`);
 });
